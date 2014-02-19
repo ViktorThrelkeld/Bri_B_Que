1cccd48bc77a82fd0411f375f725a10dfa651699
ViktorThrelkeld <Viktorthrelkeld@gmail.com>
2014-02-19 14:52:44 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BriBQue::Application.config.secret_key_base = '283ce7e8e29b6114177e5e7c7489ec97e15131dac0f9c4825a19282b6dd13acd6bfd91ebb31d32bed0f82eb03ec9ed1ba5162ed75734e2df503d680622fc4a79'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BriBQue::Application.config.session_store :cookie_store, key: '_Bri_B_Que_session'
