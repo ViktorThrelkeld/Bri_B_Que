888a805fdd2c59fbac08ec0334b156ac75cd132e
ViktorThrelkeld <Viktorthrelkeld@gmail.com>
2014-03-08 10:28:50 -0600

    WIP tests with random recipe on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def home
-    num = Recipe.first(:offset => rand(Recipe.count))
-    @recipe = Recipe.find(num)
+    @recipe = Recipe.offset(rand(Recipe.count)).first
+    # num = Recipe.first(:offset => rand(Recipe.count))
+    # @recipe = Recipe.find(num)
   end
 end
